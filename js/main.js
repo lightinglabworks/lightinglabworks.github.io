9247b1bf87a466a8c42118fbda9c9751d0f33a46
Khai Tang <khaitang@khais-mbp.attlocal.net>
2017-06-11 13:07:43 -0700

    Updates: added images, descriptions, and carousel

--- js/main.js
+++ js/main.js
@@ -91,5 +91,14 @@ $(document).ready(function() {
 		}
 	});
 
+	//Carousel
+	$('#carousel').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+		autoplaySpeed: 8000
+    });
 
 });
